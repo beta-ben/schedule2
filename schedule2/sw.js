ef0b2811ff69a1d2a337d2993ce14ab462edae45
beta-ben <beta-ben@users.noreply.github.com>
2025-08-29 00:20:16 +0000

    deploy: e1196e3439c85f0c9f4b3786b497969ca578e5e1

--- schedule2/sw.js
+++
@@ -1,7 +0,0 @@
-// Minimal no-op service worker to enable PWA install; no caching yet.
-self.addEventListener('install', (event) => {
-  self.skipWaiting()
-})
-self.addEventListener('activate', (event) => {
-  clients.claim()
-})
